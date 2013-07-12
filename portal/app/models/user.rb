7330138e87f376df6832751d620c59c40a1de53f
AndrewGoldstein <goldstein.andrew.s@gmail.com>
2013-07-12 20:13:20 +0000

    pulling facebook friends upon entry to app

--- portal/app/models/user.rb
+++ portal/app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
 
-	#serialize :friends
+	serialize :friends
 
 
 	def self.from_omniauth(auth)
@@ -9,25 +9,28 @@ class User < ActiveRecord::Base
 			user.facebook_id = auth.uid
 			user.name = auth.info.name
 			user.email = auth.info.email
-			#user.phone =
-			#user.gender = 
 			user.oauth_token = auth.credentials.token
 			user.oauth_expires_at = Time.at(auth.credentials.expires_at)
+			@graph = Koala::Facebook::API.new(user.oauth_token)
+			user.friends = @graph.get_connections("me", "friends")
+			#user.phone =
+			#user.gender = 
+			
 			user.save!
 		end
 	end
 
 
 
-	def pull_facebook_friends
-		#oauth_access_token = Koala::Facebook::OAuth.new('598601033518548', 'a091edbee36349bac46a242c6131e778', 'auth/:provider/callback')
-		#binding.pry
-		#profile = @graph.get_object("me")
-		@graph = Koala::Facebook::API.new(self.oauth_token)
-		friends = @graph.get_connections("me", "friends")
+	# def pull_facebook_friends
+	# 	#oauth_access_token = Koala::Facebook::OAuth.new('598601033518548', 'a091edbee36349bac46a242c6131e778', 'auth/:provider/callback')
+	# 	#binding.pry
+	# 	#profile = @graph.get_object("me")
+	# 	@graph = Koala::Facebook::API.new(self.oauth_token)
+	# 	friendlist = @graph.get_connections("me", "friends")
 		
-		return friends
-	end
+	# 	self.friends = friendlist
+	# end
 	
 
 
--- portal/db/schema.rb
+++ portal/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130712143038) do
+ActiveRecord::Schema.define(version: 20130712195920) do
 
   create_table "users", force: true do |t|
     t.string   "name"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20130712143038) do
     t.string   "provider"
     t.string   "oauth_token"
     t.datetime "oauth_expires_at"
+    t.string   "friends"
   end
 
 end
