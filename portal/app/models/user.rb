f9cb8e7d313fe77acc667b9faf265767b126f7c0
AndrewGoldstein <goldstein.andrew.s@gmail.com>
2013-07-12 19:57:06 +0000

    pull facebook friends

--- portal/app/models/user.rb
+++ portal/app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ActiveRecord::Base
 
+	#serialize :friends
+
+
 	def self.from_omniauth(auth)
 		where(auth.slice(:provider, :facebook_id)).first_or_initialize.tap do |user|
 			user.provider = auth.provider
@@ -13,6 +16,22 @@ class User < ActiveRecord::Base
 			user.save!
 		end
 	end
+
+
+
+	def pull_facebook_friends
+		#oauth_access_token = Koala::Facebook::OAuth.new('598601033518548', 'a091edbee36349bac46a242c6131e778', 'auth/:provider/callback')
+		#binding.pry
+		#profile = @graph.get_object("me")
+		@graph = Koala::Facebook::API.new(self.oauth_token)
+		friends = @graph.get_connections("me", "friends")
+		
+		return friends
+	end
+	
+
+
+
 
 
 
