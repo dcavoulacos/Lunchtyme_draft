6e5a8403e4241b54daf959d277306c20f2797276
dcavoulacos <d.cavoulacos@gmail.com>
2013-07-16 02:09:05 +0000

    work on schedules

--- portal/app/controllers/schedules_controller.rb
+++ portal/app/controllers/schedules_controller.rb
@@ -1,21 +1,35 @@
 class SchedulesController < ApplicationController
+	before_action :current_user
+
 	def index
-		@schedules = Schedule.where("user_id = ?", @current_user)
+		@schedules = Schedule.where("user_id = ?", @current_user.id)
 	end
 
 	def show
-		@schedule = Schedule.find(params[:id])
+		# @schedule = Schedule.find(params[:id])
 	end
 
 	def new
+		@schedule = Schedule.new
 	end
 
 	def edit		
+	end
+
+	def create		
+		@schedule = Schedule.new(schedule_params)
+
 	end
 
 	def update		
 	end
 
 	def destroy		
+	end
+
+	private
+
+	def schedule_params
+		params.require(:schedule).permit(:user_id, :day, :time, :location)
 	end
 end
--- portal/db/schema.rb
+++ portal/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130712191750) do
+ActiveRecord::Schema.define(version: 20130712195920) do
 
   create_table "schedules", force: true do |t|
     t.integer  "user_id"
