bb2a0c06393840ccbeeec7d88a7e66a92bf38de5
vagrant <vagrant@precise32.(none)>
2013-07-18 19:26:26 +0000

    nothing

--- portal/app/controllers/users_controller.rb
+++ portal/app/controllers/users_controller.rb
@@ -34,6 +34,7 @@ class UsersController < ApplicationController
     else
       @user = User.find(@id)
     end
+    render text: "You may only edit your own user details." unless @user == @current_user
   end
 
   # POST /users
@@ -41,7 +42,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     @user.netid = session[:cas_user]
-    
+
     respond_to do |format|
       if @user.save
         format.html { redirect_to @user, notice: 'User was successfully created.' }
@@ -99,6 +100,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:name,:first_name, :last_name, :netid, :class_year, :res_college, :email, :phone, :gender, :facebook_id, :friends)
+      params.require(:user).permit(:name, :first_name, :last_name, :netid, :class_year, :res_college, :email, :phone, :gender, :facebook_id, :friends)
     end
 end
