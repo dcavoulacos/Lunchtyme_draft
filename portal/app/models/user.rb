4d3fea4e37130d5ce3cd96b34aa85b7a58e7291e
AndrewGoldstein <goldstein.andrew.s@gmail.com>
2013-07-19 13:37:10 +0000

    user model, update from omniauth if loop

--- portal/app/models/user.rb
+++ portal/app/models/user.rb
@@ -12,14 +12,12 @@ class User < ActiveRecord::Base
          :conditions => "confirmed = 0"  # assuming 0 means 'pending'
 
 	def self.update_via_omniauth!(auth, user)
-		if user.facebook_id = nil		
+		if user.facebook_id == nil		
 			user.provider = auth.provider
 			user.facebook_id = auth.uid
 			user.name = auth.info.name
 			user.email = auth.info.email
-		end
-			#"https://graph.facebook.com/1463020126?fields=gender,first_name"
-		if facebook_id = auth.uid
+		elsif user.facebook_id == auth.uid
 			user.oauth_token = auth.credentials.token
 			user.oauth_expires_at = Time.at(auth.credentials.expires_at)
 			@graph = Koala::Facebook::API.new(user.oauth_token)
@@ -28,6 +26,7 @@ class User < ActiveRecord::Base
 		else
 			"You are not logged in with your own Facebook Account!"
 		end
+			#"https://graph.facebook.com/1463020126?fields=gender,first_name"
 	end
 	
 	NAME = KNOWN_AS = /^\s*Name:\s*$/i
