49247ad3e61c9f7e54f551ae75cbbb53d9c98d13
dcavoulacos <d.cavoulacos@gmail.com>
2013-07-24 16:51:45 -0400

    valid lunch matches are computed

--- portal/app/models/user.rb
+++ portal/app/models/user.rb
@@ -138,22 +138,20 @@ class User < ActiveRecord::Base
 		potential_matches = []
 		today = Time.now.strftime("%A")
 		sched1 = user.schedules.where("day = ?", today).first
-		user.matchings.find_by_status("accepted").each do |match|
+		user.matchings.where("status = ?", "accepted").each do |match|
 			
 			friend = User.find(match.match_id)
-			break unless friend.has_valid_schedule?
-			sched2 = friend.schedules.where("day = ?", today).first
-			
-			if sched1.location != sched2.location
-				break
-			elsif sched1.overlap_between_time_windows(sched1, sched2) < 30
-				break			
-			else
-				potential_matches << friend
+			if friend.has_valid_schedule?
+				sched2 = friend.schedules.where("day = ?", today).first
+				
+				if sched1.location != sched2.location
+				elsif sched1.overlap_between_time_windows(sched1, sched2) < 30	
+				else
+					potential_matches << friend
+				end
 			end
-
 		end
-		return potential_matches.first.last_name
+		return potential_matches
 	end	
 
 
