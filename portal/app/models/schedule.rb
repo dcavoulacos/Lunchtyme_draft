dcd1c74dc41b00921c0b9eec963533bdb02415a4
dcavoulacos <d.cavoulacos@gmail.com>
2013-07-24 10:59:50 -0400

    schedule sign up now updates based on day of the week

--- portal/app/models/schedule.rb
+++ portal/app/models/schedule.rb
@@ -1,3 +1,11 @@
 class Schedule < ActiveRecord::Base
   belongs_to :user
+
+  def convert_time(time_string)
+  	hour = time_string.split(':').first.to_i
+  	minute = time_string.split(':').last.to_i
+  	hour += 12 unless hour > 3
+  	return (hour*100 + minute)
+  end
+
 end
